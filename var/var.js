e0ba8e7882306b9edbbf7f462db4130020bba7d9
unknown <mahtok98212@gmail.com>
2024-07-31 11:56:35 +0530

    variable decleartion and type inference of ts

---
+++ var/var.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myVar = 3;
+console.log(myVar);
+var myHero;
+function getHero() {
+    return "Iron man";
+}
+myHero = getHero();
---
+++ var/var.ts
@@ -0,0 +1,10 @@
+let myVar: number = 3;
+console.log(myVar);
+
+let myHero;
+function getHero() {
+  return "Iron man";
+}
+myHero = getHero();
+
+export {};
